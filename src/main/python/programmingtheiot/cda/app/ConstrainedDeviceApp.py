456074ac3aba87183907472bdfb7af3acaf5bd03
antonoterof <antonoterofernandez@gmail.com>
2025-02-16 20:11:55 +0100

    Desde PIOT-CDA-02-001 hasta PIOT-CDA-02-004

--- src/main/python/programmingtheiot/cda/app/ConstrainedDeviceApp.py
+++ src/main/python/programmingtheiot/cda/app/ConstrainedDeviceApp.py
@@ -14,6 +14,8 @@ import logging
 
 from time import sleep
 
+from programmingtheiot.cda.system.SystemPerformanceManager import SystemPerformanceManager
+
 logging.basicConfig(format = '%(asctime)s:%(name)s:%(levelname)s:%(message)s', level = logging.DEBUG)
 
 class ConstrainedDeviceApp():
@@ -29,8 +31,8 @@ class ConstrainedDeviceApp():
 		@param path The name of the resource to apply to the URI.
 		"""
 		logging.info("Initializing CDA...")
-		
-		# TODO: implementation here
+
+		self.sysPerfMgr = SystemPerformanceManager()
 
 	def startApp(self):
 		"""
@@ -39,7 +41,7 @@ class ConstrainedDeviceApp():
 		"""
 		logging.info("Starting CDA...")
 		
-		# TODO: implementation here
+		self.sysPerfMgr.startManager()
 		
 		logging.info("CDA started.")
 
@@ -50,7 +52,7 @@ class ConstrainedDeviceApp():
 		"""
 		logging.info("CDA stopping...")
 		
-		# TODO: implementation here
+		self.sysPerfMgr.stopManager()
 		
 		logging.info("CDA stopped with exit code %s.", str(code))
 		
